780a0ecfc9b0e0ed582f571e5e827a1c6cbd5849
Eric <Eric.Hu214@outlook.com>
2017-11-05 01:04:40 -0500

    moved deck control to javascript and implemented showing an overlay when clicking a deck

--- public/js/user.js
+++ public/js/user.js
@@ -1,8 +1,12 @@
 var groups = ["BIO100", "BIO101", "BIO102", "BIO103", "BIO104", "BIO105"];
 
 
+var decks = {"BIO100 test 1":["hi"], "BIO100 test 2":["hello"], "BIO100 test 3":["hello"], "BIO100 final": ["hello"]};
+
+
 $(function() {
 	makeGroupList();
+	makeDecks();
 });
 
 
@@ -11,3 +15,22 @@ function makeGroupList() {
 		$('#group-list').append("<li><a href='/group' class='group'><span><strong>" + groups[index] + "</strong></span></a></li>");
 	}
 }
+
+function makeDecks() {
+	for (deck in decks) {
+		$('#deck-list').append("<div id='" + deck + "' class='cards' onclick='showOverlay(this.id)'><button><h1><strong>" + deck + "</strong></h1></button></div>");
+	}
+}
+
+function showOverlay(id) {
+	document.getElementById("deck-overlay").style.display = "block";
+	
+	var listOfCards = decks[id];
+
+	
+	for (index in listOfCards) {
+		console.log(listOfCards[index]);
+		$('#catalog').append("<li class='card' id='" + listOfCards[index] + "'><h1><strong>" + listOfCards[index] + "</strong></h1></li>");
+	}
+}
+
