513e34b61e53c49affdcd6cfd89ca59373cb2e0b
Michael Liu <michaelzb.liu@gmail.com>
2017-11-06 19:12:32 -0500

    started on the flipping of the cuecard to the cuecar back

---
+++ public/js/cue.js
@@ -0,0 +1,9 @@
+document.querySelector(".card-flip").classList.toggle("flip");
+
+
+$(document).ready(function() {
+    $('#revealAnswer').submit(function () {
+        alert("revealing answer")
+        return false
+    });
+});
