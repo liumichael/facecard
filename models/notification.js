2152e9399c6838bcebe1e2407630050c70e5957b
jenachoi <jena.choi1106@gmail.com>
2017-11-24 22:46:09 -0500

    added group model and notification model

---
+++ models/notification.js
@@ -0,0 +1,20 @@
+// app/models/notification.js
+// load the things we need
+var mongoose = require('mongoose');
+var User = require('./users.js');
+var UserSchema = mongoose.model('User').schema;
+
+// define the schema for our group model
+var notification = mongoose.Schema({
+
+    id: Number,
+    sender: [UserSchema],
+    title: String,
+    content: String,
+
+});
+
+// create the model for notification and generalize it to our app
+const notificationModel = mongoose.model('Notification', notification);
+
+module.exports = notificationModel;
